a4932f84bc3c2ddd8cbf472251c96af56c5ac083
KenethSandoval <kenethra.74@gmail.com>
2021-07-02 00:39:46 -0600

    feat: updated

---
+++ qtile/settings/groups.py
@@ -0,0 +1,21 @@
+# Qtile workspaces
+
+from libqtile.config import Key, Group
+from libqtile.command import lazy
+from .keys import mod, keys
+
+
+# Get the icons at https://www.nerdfonts.com/cheat-sheet (you need a Nerd Font)
+
+groups = [Group(i) for i in [
+    "   ", "   ", "   ", "  ", "   ", "   ", " ﭮ ",
+]]
+
+for i, group in enumerate(groups):
+    actual_key = str(i + 1)
+    keys.extend([
+        # Switch to workspace N
+        Key([mod], actual_key, lazy.group[group.name].toscreen()),
+        # Send window to workspace N
+        Key([mod, "shift"], actual_key, lazy.window.togroup(group.name))
+    ])
