b94c16c673a31eb8f3555f4335133e344f70fd3a
KenethSandoval <kenethra.74@gmail.com>
2021-09-26 14:58:12 -0600

    feat: function for key maps

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -1,6 +1,28 @@
-local map = vim.api.nvim_set_keymap
+local map = function(key)
+  -- extraer opciones
+  local opts = {noremap=true}
+  for i, v in pairs(key) do
+    if type(i) == 'string' then opts[i] = v end
+  end
 
-map('n', '<Leader>w', ':write<CR>', {noremap = true})
-map('n', '<Leader>q', ':q<CR>', {noremap = true})
-map('n', '<Leader>f', ':Telescope find_files<CR>', {noremap = true})
+  -- soporte básico para atajos de buffer
+  local buffer = opts.buffer
+  opts.buffer = nil
+
+  if buffer then
+    vim.api.nvim_buf_set_keymap(0, key[1], key[2], key[3], opts)
+  else
+    vim.api.nvim_set_keymap(key[1], key[2], key[3], opts)
+  end
+end
+
+map{'n', '<Leader>w', ':write<CR>'}
+map{'n', '<Leader>q', ':quit<CR>'}
+-- Telescope.nvim
+map{'n', '<Leader>ff', ':Telescope find_files<CR>'}
+map{'n', '<Leader>fg', ':Telescope live_grep<CR>'}
+map{'n', '<Leader>fb', ':Telescope buffers<CR>'}
+map{'n', '<Leader>fh', ':Telescope help_tags<CR>'}
+--NERDTree
+map{'n', '<Leader>b', ':NERDTreeToggle<CR>'}
 
