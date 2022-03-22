9cf40548b585fa950aa209e7d0a457a2c74d6bb1
KenethSandoval <kenethra.74@gmail.com>
2022-03-21 18:48:16 -0600

    feat: theme emacs

--- .emacs.d/elisp/base.el
+++ .emacs.d/elisp/base.el
@@ -1,8 +1,21 @@
-(package-initialize)
-(add-to-list 'package-archives
-	     '("melpa" . "https://melpa.org/packages/"))
+;(package-initialize)
+;(add-to-list 'package-archives
+;	     '("melpa" . "https://melpa.org/packages/"))
 
-(when (not package-archive-contents)
-  (package-refresh-contents))
+;(when (not package-archive-contents)
+   ;  (package-refresh-contents))
+
+
+(require 'package)
+(setq package-archives '(("melpa" . "https://melpa.org/packages/")
+                         ("org" . "https://orgmode.org/elpa/")
+                         ("elpa" . "https://elpa.gnu.org/packages/")))
+(package-initialize)
+(unless package-archive-contents
+ (package-refresh-contents))
+(unless (package-installed-p 'use-package)
+   (package-install 'use-package))
+(require 'use-package)
+(setq use-package-always-ensure t)
 
 (provide 'base)
--- .emacs.d/elisp/theme.el
+++ .emacs.d/elisp/theme.el
@@ -1,6 +1,12 @@
-(use-package jetbrains-darcula-theme
-	     :config
-	       (load-theme 'jetbrains-darcula t))
+;(use-package doom-themes
+;  :ensure t)
+
+;(setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
+;      doom-themes-enable-italic t) ; if nil, italics is universally disabled
+;(load-theme 'doom-vibrant t)
+
+(add-to-list 'custom-theme-load-path
+             "/home/stivarch/.emacs.d/elpa/gruber-darker-theme/")
+
 
 (provide 'theme)
-
