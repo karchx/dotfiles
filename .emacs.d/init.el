e4fc09196bad137ac5b6ff7953e63aae996b1eeb
Стив <kenethra.74@gmail.com>
2022-05-02 11:12:55 -0600

    feat: jtxs.el and relative number line

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,11 +1,14 @@
-(global-linum-mode t)
+;; relative number lines
+(global-display-line-numbers-mode 1)
+(setq display-line-numbers-type 'relative)
+
 (setq make-backup-files nil)
 
 (add-to-list 'load-path (concat user-emacs-directory "elisp"))
 
 ;;No menu bar
 (menu-bar-mode -1)
-(tool-bar-mode -1)
+;(tool-bar-mode -1)
 
 (show-paren-mode t)
 (electric-pair-mode 1)
@@ -16,8 +19,8 @@
 (require 'c-cpp)
 (require 'gopls)
 (require 'python)
-(require 'jtxs)
-(require 'modeline)
+;(require 'jtxs)
+(add-to-list 'auto-mode-alist '("\\.ts\\'" . typescript-mode))
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
@@ -28,7 +31,7 @@
  '(custom-safe-themes
    '("e13beeb34b932f309fb2c360a04a460821ca99fe58f69e65557d6c1b10ba18c7" default))
  '(package-selected-packages
-   '(use-package evil lsp-mode yasnippet lsp-treemacs helm-lsp projectile hydra flycheck company avy which-key helm-xref dap-mode tide)))
+   '(use-package evil lsp-mode yasnippet lsp-treemacs helm-lsp projectile hydra flycheck company avy which-key helm-xref dap-mode)))
 
 (when (cl-find-if-not #'package-installed-p package-selected-packages)
   (package-refresh-contents)
